0a529975b3a156e612692092455d2b93d595aa55
Matt Briggs <matt@mattbriggs.net>
2011-11-22 01:05:17 -0500

    initial

---
+++ rails-test-toggler.el
@@ -0,0 +1,152 @@
+;;; rails-test-toggler.el -- Flip between test and implementation in a rails project
+
+;; Copyright (C) 2011 Matt Briggs
+;;
+;; Author: Matt Briggs <matt@mattbriggs.net>
+;; URL: http://github.com/mbriggs/rails-test-toggler
+;; Version: 0.1
+;; Keywords: rails
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Provides a way to quickly toggle between test and implementation
+
+;;; Installation:
+
+;; Put rails-test-toggler.el somewhere in your load path, and
+
+;; (require 'rails-test-toggler)
+
+;; in your init.el. To toggle between test and implementation, bind
+;; rtt/toggle-test-and-implementation to something.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;; Code
+
+(defun rtt/toggle-test-and-implementation ()
+  (interactive)
+  (let ((file (buffer-file-name)))
+    (if (rtt/test? file)
+        (rtt/find-test-by-target-type file)
+        (rtt/find-target-by-test-type file))))
+
+(defun rtt/find-test-by-target-type (test-file)
+  (cond ((rtt/helper-test? test-file) (rtt/find-helper-test test-file))
+        ((rtt/unit-test? test-file) (rtt/find-unit-test test-file))
+        ((rtt/functional-test? test-file) (rtt/find-functional-test test-file))))
+
+(defun rtt/find-target-by-test-type (test-file)
+  (cond ((rtt/helper? test-file) (rtt/find-helper test-file))
+        ((rtt/model? test-file) (rtt/find-model test-file))
+        ((rtt/controller? test-file) (rtt/find-controller test-file))))
+
+
+
+;;;;;;;;;;;;;;;;
+;; Predicates ;;
+;;;;;;;;;;;;;;;;
+
+(defun rtt/test? (file)
+  (string-match "\\/test\\/" file))
+
+(defun rtt/model? (file)
+  (or
+   (string-match "\\/app\\/models" file)
+   (string-match "\\/lib\\/" file)))
+
+(defun rtt/helper? (file)
+  (string-match "\\/app\\/helpers" file))
+
+(defun rtt/controller? (file)
+  (string-match "\\/app\\/controllers" file))
+
+(defun rtt/unit-test? (file)
+  (string-match "\\/test\\/unit" file))
+
+(defun rtt/helper-test? (file)
+  (string-match "\\/test\\/unit\\/helpers" file))
+
+(defun rtt/functional-test? (file)
+  (string-match "\\/test\\/functional" file))
+
+
+
+
+;;;;;;;;;;;;;
+;; Finders ;;
+;;;;;;;;;;;;;
+
+(defun rtt/find-controller (file)
+  (rtt/find-target "app\\/controllers" "functional" file))
+
+(defun rtt/find-helper (file)
+  (rtt/find-target "app\\/helpers" "unit/helpers" file))
+
+(defun rtt/find-model (file)
+  (or
+   (rtt/find-target "app\\/models" "unit" file)
+   (rtt/find-target "lib" "unit" file)))
+
+(defun rtt/find-functional-test (test-file)
+  (rtt/find-test "functional" "app/controllers" test-file))
+
+(defun rtt/find-helper-test (test-file)
+  (rtt/find-test "unit\\/helpers" "app/helpers" test-file))
+
+(defun rtt/find-unit-test (test-file)
+  (or
+   (rtt/find-test "unit" "app/models" test-file)
+   (rtt/find-test "unit" "lib" test-file)))
+
+
+
+
+;;;;;;;;;;;;;
+;; Helpers ;;
+;;;;;;;;;;;;;
+
+(defun rtt/find-test (test-re rails-dir test-file)
+  (let ((path (rtt/test-file test-re rails-dir test-file)))
+    (if (file-exists-p path)
+        (find-file path))))
+
+(defun rtt/find-target (rails-dir-re test-dir file)
+  (let ((path (rtt/target-file rails-dir-re test-dir file)))
+    (if (file-exists-p path)
+        (find-file path))))
+
+(defun rtt/test-file (test-re rails-dir test-file)
+  (replace-regexp-in-string (concat "test\\/" test-re)
+                            rails-dir
+                            (drop-test-extension test-file)))
+
+(defun rtt/target-file (rails-dir-re test-dir file)
+  (replace-regexp-in-string rails-dir-re
+                            (concat "test/" test-dir)
+                            (add-test-extension file)))
+
+(defun rtt/drop-test-extension (file)
+  (replace-regexp-in-string "_test" "" file))
+
+(defun rtt/add-test-extension (file)
+  (replace-regexp-in-string "\\.rb" "_test.rb" file))
+
+(provide 'rails-test-toggler)
